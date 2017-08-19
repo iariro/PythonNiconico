eba7e677e6ab412ad566e9287224d79c4ff6f2f0
kumagai <kumagai@kumagai-no-MacBook-Air-2.local>
2017-08-19 12:41:33 +0900

    new

---
+++ nicocrawler.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import datetime
+import json
+import re
+import subprocess
+
+# read json
+file = open('nicocrawler.json', 'r')
+data = json.load(file)
+file.close()
+
+# update datetime
+pdatetime = "-"
+datetime = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+if data.has_key('datetime'):
+	pdatetime = data['datetime']
+print '%s -> %s' % (pdatetime, datetime)
+data['datetime'] = datetime 
+
+userComments = r'.* ([0-9]*) UserComments.*'
+
+for site in data['sites']:
+	# get HTML
+	url = 'http://www.nicovideo.jp/watch/%s' % site['url']
+	curlCommand = [ 'curl', '-s',  url]
+	res = subprocess.check_output(curlCommand)
+
+	# get UserComments count
+	lines = res.split('\n')
+	for line in lines:
+		if 'UserComments' in line:
+			commentCount = re.sub(userComments, r'\1', line)
+
+			# update count
+			if site.has_key('commentCount') == False:
+				site['commentCount'] = 0
+			if commentCount != site['commentCount']:
+				print "%s %s->%s" % (site['title'], site['commentCount'], commentCount)
+			site['commentCount'] = commentCount
+
+# write json
+file = open('nicocrawler.json', 'w')
+json.dump(data, file, indent=4)
+file.close()
